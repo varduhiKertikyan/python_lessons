d92c5a4db221f80736375c7dd9f37b3a37afbd86
varduhikertikyan <varduhi.kertikyan@picsart.com>
2022-06-23 17:18:29 +0400

    test

--- practice2.py
+++ practice2.py
@@ -2,10 +2,10 @@ def unique_words():
     word_list = []
     while 1:
         typed_word = input("type a word: ")
-        if typed_word == "":
-            break
-        else:
+        if typed_word:
             word_list.append(typed_word)
+        else:
+            break
     unique_word_list = []
     for word in word_list:
         if word not in unique_word_list:
@@ -69,8 +69,63 @@ def is_palindrome1(str):
 #print(is_palindrome1("Dcc bab, ccd"))
 
 def input_numbers():
+    list_of_inputs = []
+    sum = 0
+    count = 0
     while True:
-        if not int(input("type number: ")):
-            return
+        number = input("type a number: ")
+        if number:
+            sum += int(number)
+            list_of_inputs.append(number)
+            count += 1
+        else:
+            break
+    middle = sum / count
+    print(middle)
+    for num in list_of_inputs:
+        if num < middle:
+            print(num)
+        if num == middle:
+            print(num)
+        if num > middle:
+            print(num)
 
 #input_numbers()
+
+
+import random
+def lottery():
+    result = []
+    for i in range(6):
+        x = random.randint(1, 49)
+        if x not in result:
+            result.append(x)
+    return sorted(result)
+
+#print(lottery())
+
+def is_sorted():
+    result = [int(x) for x in input("input nums: ").split()]
+    print(result)
+    if sorted(result) == result:
+        return True
+    elif sorted(result)[::-1]==result:
+        return True
+    else:
+        return False
+
+#print(is_sorted())
+
+def  is_sublist(larger, smaller):
+    flag = True
+    for i in range(len(smaller)):
+        try:
+            x = larger.index(smaller[i])
+            if x + 1 !=  larger.index(smaller[i+1]):
+                flag = False
+        except:
+            pass
+    return flag
+
+
+#print(is_sublist([1, 2, 3, 4], [2, 4]))
